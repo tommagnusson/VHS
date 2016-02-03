19332fcde40c22f4a0c72041622ad2844b778d9e
Tommy <tommagnuss@gmail.com>
2016-02-03 16:17:50 -0500

    Activity 2 for Elevens lab

---
+++ vhs/src/vhs/elevens/DeckTester.java
@@ -0,0 +1,30 @@
+package vhs.elevens;
+
+import java.util.Arrays;
+
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *
+	 * @param args
+	 *            is not used.
+	 */
+	public static void main(String[] args) {
+		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
+
+		String[] ranks = { "Ace", "Two", "Three", "Four", "Five", "Six",
+				"Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+
+		int[] pointValues = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10 };
+
+		Deck deck = new Deck(ranks, suits, pointValues);
+		System.out.println(deck);
+		System.out.println("Dealt: " + deck.deal());
+		System.out.println(deck.isEmpty());
+		System.out.println(deck.size());
+	}
+}
